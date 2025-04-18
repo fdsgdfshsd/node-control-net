118dfe71d3fd60cb8192d9ed3f5f83feb27e9d89
Gera <mga310507@gmail.com>
2025-04-18 17:45:44 +0300

    prestart

--- __init__.py
+++ __init__.py
@@ -1,7 +1,7 @@
-try:
-    from . import prestartup_script
-except Exception as e:
-    print(f"Ошибка при выполнении prepare_environment: {e}")
+# try:
+#     from . import prestartup_script
+# except Exception as e:
+#     print(f"Ошибка при выполнении prepare_environment: {e}")
 
 import sys, os
 from .utils import here, define_preprocessor_inputs, INPUT
